97ff687fcf342552f51f4bf0ba2a379f10d08ebe
zhangzhen <zz409119@126.com>
2018-10-31 00:12:00 +0800

    complete chapter14

---
+++ src/chapter14/DisplayClock.java
@@ -0,0 +1,33 @@
+package chapter14;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class DisplayClock extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		ClockPane clock = new ClockPane();
+		String timeString = clock.getHour() + ":" + clock.getMinute() + ":" + clock.getSecond();
+		Label lblCurrentTime = new Label(timeString);
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(clock);
+		pane.setBottom(lblCurrentTime);
+		BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+		
+		Scene scene = new Scene(pane);
+		primaryStage.setTitle("DisplayClock");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
